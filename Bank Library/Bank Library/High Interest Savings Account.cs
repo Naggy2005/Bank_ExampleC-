5aff167e1ca5fce50c7322bc3e7600e7ae99cc73
Naggy2005 <156240986+Naggy2005@users.noreply.github.com>
2025-01-21 20:25:23 -0500

    Bank Example
    Part of the bank Example done

---
+++ Bank Library/Bank Library/High Interest Savings Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Library
+{
+    public class HighInterestSavingsAccount : SavingsAccount
+    {
+        public HighInterestSavingsAccount(string accountNumber, string accountName, decimal initialBalnce, decimal interestRate) :
+            base(accountNumber, accountName, initialBalnce, interestRate)
+        {
+
+        }
+        public void ApplybonusInterest(decimal bonusRate)
+        {
+            decimal bonusInterestPayment = this.Balance * bonusRate;
+            this.Deposit(bonusInterestPayment);
+        }
+    }
+}
